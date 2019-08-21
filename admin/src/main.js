bc821478df012cf60772e76e44b385b4509da3a2
xiaohuihuiovo <1666352306@qq.com>
2019-08-21 16:39:13 +0800

    商品分类功能

--- admin/src/main.js
+++ admin/src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import './plugins/element.js'
 import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
+
+
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
@@ -16,6 +19,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
