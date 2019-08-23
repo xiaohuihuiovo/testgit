62d0d2acc70bdd01a9d8065552594ec22d2e0991
xiaohuihuiovo <1666352306@qq.com>
2019-08-23 14:05:58 +0800

    数据报表完成

--- admin/src/router.js
+++ admin/src/router.js
@@ -11,6 +11,7 @@ import Params from './components/goods/Params.vue'
 import GoodList from './components/goods/List.vue'
 import Add from './components/goods/Add.vue'
 import Order from './components/order/Order.vue'
+import Report from './components/report/Report.vue'
 
 Vue.use(Router)
 
@@ -55,6 +56,10 @@ const router = new Router({
         {
           path: '/orders',
           component: Order
+        },
+        {
+          path: '/reports',
+          component: Report
         }
       ]
     }
